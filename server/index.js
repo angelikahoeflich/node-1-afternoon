033ff6511927b0842bae999632df4ce566adcffe
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-26 13:51:28 -0700

    hey cool it worked!

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const products = require('../products.json');
+const getProducts = require('./getProducts');
+const getProduct = require('./getProduct')
 
 const app = express();
 
 app.listen(8080, () => console.log('listening on port 8080'));
+
+app.get('/api/products', getProducts);
+
+app.get('/api/products/:id', getProduct) 
